969963a8478afce8b17cc03cd054be3ece4c11f3
Vivek Joshi <vivekjoshi15@gmail.com>
2016-12-14 19:01:57 +0530

    changing config details for db and smtp server

--- classes/development.js
+++ classes/development.js
@@ -4,9 +4,15 @@ module.exports = {
 		token: '44de6f566bda02a358d850be8da6900e',
 		from: '19544407348'
 	},mysql: {
-		host     : '127.0.0.1',
-		user     : 'root',
-		password : 'root',
-		database : 'db_naan'
+		host     : 'us-cdbr-iron-east-04.cleardb.net',
+		user     : 'bbf17523027856',
+		password : 'ff4d2a6c',
+		database : 'heroku_b37fff5796a819f'
+	},smtp: {
+		apikey     : 'key-8da93bbc51c1fd6f92d0de0e6b6ea3b9',
+		user     : 'postmaster@app6b366b66c2db417b8f3347a30ad6bbe1.mailgun.org',
+		password : '5d58ea3bdd8335db7b163d65dc215878',
+		host: 'smtp.mailgun.org',
+		domain: 'app6b366b66c2db417b8f3347a30ad6bbe1.mailgun.org'
 	}
 };
