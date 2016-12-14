75a658a6f18f4327dada50142acaaadf3821cbe9
Vivek Joshi <vivekjoshi15@gmail.com>
2016-12-14 21:50:27 +0530

    fixing port in app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(fileUpload());
 app.use(Express.static(__dirname + '/public'));
 app.use('/static', Express.static(__dirname + '/public'));
 app.enable('trust proxy');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 
 app.use(session({
   cookieName: 'session',
